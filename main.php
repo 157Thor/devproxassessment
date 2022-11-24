af5e20c7b264606a80597a0c03d592079827df42
157Thor <jordan@ifalk.com>
2022-11-24 21:32:14 +0200

    added a lot of validation

--- main.php
+++ main.php
@@ -3,6 +3,10 @@
 
 <head>
     <title>DEVPROX Assessment</title>
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 </head>
 
 <body>
@@ -16,13 +20,10 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
 
     // Create database
     $sql = "CREATE DATABASE IF NOT EXISTS myDB";
-    if ($conn->query($sql) === TRUE) {
-        echo "Database created successfully";
-    } else {
+    if ($conn->query($sql) !== TRUE) {
         echo "Error creating database: " . $conn->error;
     }
 
@@ -33,55 +34,106 @@
         date_of_birth DATE NOT NULL
     )";
 
-    if ($conn->query($sql) === TRUE) {
-        echo "myTable created successfully";
-    } else {
+    if ($conn->query($sql) !== TRUE) {
         echo "Error creating myTable: " . $conn->error;
     }
-    
+
     $conn->close();
     $conn = new mysqli($servername, $username, $password, 'myDB');
-
+    $name = "";
+    $surname = "";
+    $id = "";
+    $id_error = "";
+    $date = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = $_REQUEST['name'];
         $surname = $_REQUEST['surname'];
         $id = $_REQUEST['id'];
         $date = $_REQUEST['date'];
-        $sql = "INSERT INTO myTable VALUES (
-            '$id', '$name', '$surname', '$date'
-            )";
-        echo $sql;
-        if ($conn->query($sql) === TRUE) {
-            echo "Record entered successfully";
+        $result = $conn->query("SELECT * FROM myTable WHERE id = $id");
+        if ($result->num_rows > 0) {
+            $id_error = "This ID number already exists!";
         } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            $sql = "INSERT INTO myTable VALUES (
+                '$id', '$name', '$surname', STR_TO_DATE('$date', '%d/%m/%Y')
+                )";
+            if ($conn->query($sql) === TRUE) {
+                echo "Record entered successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
         }
-        
     }
+    $conn->close();
     ?>
     <h1>DEVPROX Assessment</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" onsubmit="return validateForm()">
         <label for="name">Name:</label>
-        <input type="text" name="name">
+        <input type="text" name="name" id="name" value="<?php echo $name ?>">
+        <label for="" id="name-error"></label>
         <br>
         <label for="surname">Surname:</label>
-        <input type="text" name="surname">
+        <input type="text" name="surname" id="surname" value="<?php echo $surname ?>">
+        <label for="" id="surname-error"></label>
         <br>
         <label for="id">ID:</label>
-        <input type="text" name="id">
+        <input type="text" name="id" id="id" value="<?php echo $id ?>">
+        <label for="" id="id-error"><?php echo $id_error ?></label>
         <br>
+        <!-- I make the date field readonly and use jQuery datepicker for validation -->
         <label for="date">Date of Birth:</label>
-        <input type="date" name="date" placeholder="dd/mm/yyyy" min="1921-01-01" max="2021-12-31">
+        <input type="text" name="date" id="date" readonly="readonly" value="<?php echo $date ?>">
+        <label for="" id="date-error"></label>
         <br>
         <button type="submit">Submit</button>
         <button type="reset">Cancel</button>
     </form>
-    <?php
+    <script>
+        $("#date").datepicker({
+            dateFormat: "dd/mm/yy",
+            minDate: "01/01/1920",
+            maxDate: -1,
+            changeMonth: true,
+            changeYear: true,
+            yearRange: "1920:2022",
+        });
 
-    $conn->close();
-
-    ?>
-
+        function validateForm() {
+            var id = $("#id").val();
+            var date = $("#date").val();
+            var name = $("#name").val();
+            var surname = $("#surname").val();
+            var flag = true;
+            if (!isNaN(name)) {
+                $("#name-error").text("Name is invalid!")
+                flag = false;
+            } else {
+                $("#name-error").empty();
+            }
+            if (!isNaN(surname)) {
+                $("#surname-error").text("Surname is invalid!")
+                flag = false;
+            } else {
+                $("#surname-error").empty();
+            }
+            if (id.length != 13 || isNaN(id)) {
+                $("#id-error").text("This ID number is invalid!");
+                flag = false;
+            } else {
+                $("#id-error").empty();
+            }
+            if (!date || date == "") {
+                $("#date-error").text("Please enter a date!");
+                flag = false;
+            } else if (id.substr(0, 2) != date.substr(8, 2) || id.substr(2, 2) != date.substr(3, 2) || id.substr(4, 2) != date.substr(0, 2)) {
+                $("#date-error").text("Your Date of Birth doesn't match your ID number!");
+                flag = false;
+            } else {
+                $("#date-error").empty();
+            }
+            return flag;
+        }
+    </script>
 </body>
 
 </html>
